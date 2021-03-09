6d79e6ef8c9639483d507d4a65d33a06406e6555
Kononkov1998 <kononkov98@mail.ru>
2021-03-09 17:53:56 +0700

    init

---
+++ fabfile.py
@@ -0,0 +1,31 @@
+from fabric.api import local
+
+
+def run(port=8000):
+    local(f"python manage.py runserver 0.0.0.0:{port}")
+
+
+def upgrade():
+    local("python -m pip install -U pip")
+    local("pip install --upgrade -r requirements.txt")
+
+
+def migrate():
+    local("python manage.py makemigrations")
+    local("python manage.py migrate")
+
+
+def makemigrations():
+    local("python manage.py makemigrations")
+
+
+def test():
+    local("python manage.py test")
+
+
+def startapp(name):
+    local(f"python manage.py startapp {name}")
+
+
+def check():
+    local(f"python manage.py check --deploy")
